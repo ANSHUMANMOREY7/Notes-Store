2e04bc2b1d8f871636c301a9ec1b8026e9cae9a4
Anshuman Morey <anshumanmorey2@gmail.com>
2026-03-14 14:09:16 +0530

    navbar added

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import { memo } from 'react';
+
+const Navbar = () => {
+  return (
+    <div>
+      <h2 className='font-[font1] font-bold text-black fixed z-0'>Notes Store</h2>
+    </div>
+  );
+};
+
+export default memo(Navbar);
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -2,16 +2,21 @@ import { memo } from 'react';
 import Video from './Video';  
 import HomeHeroText from './HomeHeroText';
 import HomeBottomText from './HomeBottomText';
+import Navbar from '../../components/Navbar/Navbar';
 const Home = () => {
   return (
+    <div>
+      <Navbar />
     <div className='overflow-x-hidden overflow-y-auto'>
       <div className='h-screen w-screen absolute top-0  '>
-        <Video />
+        {/* <Video /> */}
         </div>
         <div className='w-screen relative overflow-x-hidden '>
         <HomeHeroText />
         <HomeBottomText />
       </div>
+    </div>
+    
     </div>
   );
 };
