44e866a067a631231cd1baa288b8dea86c283aae
Anshuman Morey <anshumanmorey2@gmail.com>
2026-03-14 16:41:54 +0530

    2nd home page completed

--- client/src/pages/Home/HomeBottomText.jsx
+++ client/src/pages/Home/HomeBottomText.jsx
@@ -2,16 +2,16 @@ import { memo } from 'react';
 
 const HomeBottomText = () => {
   return (
-    <div className='w-screen h-screen bg-black text-white flex '>
+    <div className='w-screen h-screen bg-black text-white p-10 flex flex-col justify-center items-start '>
       <div>
-      <h1 >Our Mission</h1>
+      <h2 className='font-[font1] text-white text-7xl p-10 '>Our Mission</h2>
 
       </div>
-      <div>
+      <div className='font-[font1] px-10  text-3xl font-bold text-justify'>
         <h2>Providing a singular, reliable source for verified study materials.</h2>
       </div>
-      <div>
-       <p>"We provide a centralized, secure repository for MCA students to access high-quality academic resources. By centralizing materials under faculty supervision, we eliminate the inefficiency of fragmented study notes and ensure that every resource—from lecture modules to practical lab manuals—remains fully aligned with the current university curriculum."</p>
+      <div className='font-[font1] px-10 py-10 text-1xl text-justify'>
+       <p>" We provide a centralized, secure repository for MCA students to access high quality academic resources. By centralizing materials under faculty supervision, we eliminate the inefficiency of fragmented study notes and ensure that every resource from lecture modules to practical lab manuals remains fully aligned with the current university curriculum."</p>
       </div>
     </div>
   );
