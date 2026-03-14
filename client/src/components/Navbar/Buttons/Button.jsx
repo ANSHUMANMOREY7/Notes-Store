95c39c8481d9bdf05bb6d28e1e27ffa3a1ba442a
Anshuman Morey <anshumanmorey2@gmail.com>
2026-03-14 17:06:42 +0530

    button added

---
+++ client/src/components/Navbar/Buttons/Button.jsx
@@ -0,0 +1,14 @@
+import { memo } from 'react';
+import './Button.css';
+const Button = () => {
+  return (
+    <div>
+        <div>
+      <h2 className='button'>Button</h2>
+
+        </div>
+    </div>
+  );
+};
+
+export default memo(Button);
--- client/src/pages/Home/HomeBottomText.jsx
+++ client/src/pages/Home/HomeBottomText.jsx
@@ -1,5 +1,5 @@
 import { memo } from 'react';
-
+import Button from '../../components/Navbar/Buttons/Button';
 const HomeBottomText = () => {
   return (
     <div className='w-screen h-screen bg-gradient-to-b from-black to-purple-900 text-white p-10 flex flex-col justify-center items-start relative overflow-hidden'>
@@ -27,7 +27,9 @@ const HomeBottomText = () => {
       <div className='font-[font1] px-10 py-10 text-1xl text-justify'>
        <p>" We provide a centralized, secure repository for MCA students to access high quality academic resources. By centralizing materials under faculty supervision, we eliminate the inefficiency of fragmented study notes and ensure that every resource from lecture modules to practical lab manuals remains fully aligned with the current university curriculum."</p>
       </div>
+     <Button />
     </div>
+    
   );
 };
 
