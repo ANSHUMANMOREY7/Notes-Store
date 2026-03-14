c25ea0c362414a03198cfcec2a8bc231cb814deb
Anshuman Morey <anshumanmorey2@gmail.com>
2026-03-14 22:06:09 +0530

    add admin login title

--- client/src/pages/Portals/AdminLogin.jsx
+++ client/src/pages/Portals/AdminLogin.jsx
@@ -7,7 +7,11 @@ const AdminLogin = () => {
       <Navbar />
       <div className="min-h-screen flex items-center justify-center bg-gradient-to-b from-black to-purple-900 text-white">
    <div className="bg-black/70 p-10 rounded-2xl w-96">
-  Admin Login Page
+
+  <h2 className="text-2xl font-bold text-center">
+    Admin Login
+  </h2>
+
 </div>
   </div>
     
