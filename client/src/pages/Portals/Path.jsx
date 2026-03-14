32476ca15c4806ffb4605d674022edb060389289
Anshuman Morey <anshumanmorey2@gmail.com>
2026-03-14 18:33:19 +0530

    gradient added

--- client/src/pages/Portals/Path.jsx
+++ client/src/pages/Portals/Path.jsx
@@ -1,17 +1,18 @@
 import { memo } from 'react';
 import Navbar from '../../components/Navbar/Navbar';
 import './path.css';
+import Button from '../../components/Navbar/Buttons/Button';
 
 const Path = () => {
   return (
 
    <div className='main-container font-[font1] font-bold'>
     <Navbar />
-  <div className='student-sec'>
-    <h1>Login as Student</h1>
+  <div className='student-sec '>
+    <h1> <Button > </Button></h1>
   </div>
   <div className='admin-sec'>
-    <h1>Login as Admin</h1>
+    <h1> <Button > </Button></h1>
   </div>
 </div>
 )
