bea2c7bc1b1a234c4385a4be0e9c1f8a3387790d
Anshuman Morey <anshumanmorey2@gmail.com>
2026-03-14 18:23:36 +0530

    path pages added

--- client/src/pages/Portals/Path.jsx
+++ client/src/pages/Portals/Path.jsx
@@ -1,12 +1,20 @@
 import { memo } from 'react';
-
+import Navbar from '../../components/Navbar/Navbar';
+import './path.css';
 
 const Path = () => {
   return (
-    <div>
-      <h2>Pathh</h2>
-    </div>
-  );
+
+   <div className='main-container font-[font1] font-bold'>
+    <Navbar />
+  <div className='student-sec'>
+    <h1>Login as Student</h1>
+  </div>
+  <div className='admin-sec'>
+    <h1>Login as Admin</h1>
+  </div>
+</div>
+)
 };
 
 export default memo(Path);
