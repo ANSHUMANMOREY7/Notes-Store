e85ea51860561658403d265df9ac4d685cec789c
Anshuman Morey <anshumanmorey2@gmail.com>
2026-03-15 18:44:13 +0530

    contact info added in footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,9 @@
 import { memo } from 'react';
-
+import { FaInstagram, FaLinkedin, FaEnvelope } from 'react-icons/fa';
 const Footer = () => {
 return (
     <footer className="bg-black text-white py-10 mt-auto">
-      <div className="max-w-7xl mx-[50px] px-4 sm:px-6 lg:px-8 grid grid-cols-1 md:grid-cols-3 gap-10">
+      <div className="max-w-[1200px] mx-auto px-4 sm:px-6 lg:px-8 grid grid-cols-1 md:grid-cols-4 gap-16">
         
         
         <div className=''>
@@ -13,7 +13,7 @@ return (
 
         
         <div>
-          <h3 className="text-white font-semibold mb-4">Quick Links</h3>
+          <h3 className="text-white font-semibold mb-4 text-[25px]">Quick Links</h3>
           <ul className="space-y-2 text-sm">
             <li><a href="/" className="hover:text-purple-500 transition">Home</a></li>
             <li><a href="/about" className="hover:text-purple-500 transition">About</a></li>
@@ -23,14 +23,24 @@ return (
 
         
         <div>
-          <h3 className="text-white font-semibold mb-4">Support</h3>
+          <h3 className="text-white font-semibold mb-4 text-[25px]">Support</h3>
           <ul className="space-y-2 text-sm">
             <li><a href="/privacy" className="hover:text-purple-500 transition">Privacy Policy</a></li>
             <li><a href="/terms" className="hover:text-purple-500 transition">Terms of Service</a></li>
-            <li><a href="mailto:support@notesstore.com" className="hover:text-purple-500 transition">support@notesstore.com</a></li>
+            <li><a href="" className="hover:text-purple-500 transition">support@notesstore.com</a></li>
           </ul>
         </div>
+        <div>
+          <h3 className="text-white font-semibold mb-4 text-[22px]">Connect With Us</h3>
+          <div className="flex space-x-4">
+            <a href="https://mail.google.com/mail/u/0/#inbox" className="hover:text-red-400 text-xl"><FaEnvelope /></a>
+            <a href="https://www.instagram.com/anshumxnn_27/?hl=en" target="_blank" rel="noreferrer" className="hover:text-pink-500 text-xl"><FaInstagram /></a>
+            <a href="https://www.linkedin.com/in/anshuman-morey/" target="_blank" rel="noreferrer" className="hover:text-blue-500 text-xl"><FaLinkedin /></a>
+          </div>
+        </div>
       </div>
+      
+      
       
       <div className="text-center mt-10 pt-6 border-t border-gray-800 text-xs">
         &copy; {new Date().getFullYear()} Notes Store. All rights reserved.
