f666c16df8b752fe5c7e356c30e10712bceae393
Anshuman Morey <anshumanmorey2@gmail.com>
2026-03-15 13:37:00 +0530

    stats card added with animate plus class in all the icons in cards

--- client/src/pages/Portals/Admin.jsx
+++ client/src/pages/Portals/Admin.jsx
@@ -33,7 +33,7 @@ const Admin = () => {
             </button>
           </div>
 
-          <div className="flex flex-wrap justify-center gap-10 mt-10">
+          <div className="flex  justify-center gap-10 mt-10">
             <div className="bg-black/70 backdrop-blur-md text-white w-96 p-10 rounded-2xl flex flex-col items-center text-center gap-6 shadow-xl hover:scale-105 transition duration-300 hover:bg-purple-900 group border border-white/5">
               <div className="w-16 h-16 bg-purple-600/20 rounded-full flex items-center justify-center text-4xl animate-pulse group-hover:animate-none group-hover:scale-110 transition-transform">
                 ➕
@@ -47,19 +47,33 @@ const Admin = () => {
                 Start Upload
               </button>
             </div>
+
             <div className="bg-black/70 backdrop-blur-md text-white w-96 p-10 rounded-2xl flex flex-col items-center text-center gap-6 shadow-xl hover:scale-105 transition duration-300 hover:bg-blue-900 group border border-white/5">
-  <div className="w-16 h-16 bg-blue-600/20 rounded-full flex items-center justify-center text-4xl group-hover:scale-110 transition-transform">
-    📝
-  </div>
-  <h2 className="text-3xl font-bold tracking-tight font-[font1]">Manage Notes</h2>
-  <p className="text-gray-300 font-[font1]">Edit details or delete outdated study materials from the library.</p>
-  <button 
-    onClick={() => alert("Manage Notes")}
-    className="bg-blue-600 hover:bg-blue-700 px-8 py-3 rounded-lg font-bold transition duration-300 shadow-lg shadow-blue-500/20 font-[font1] w-full cursor-pointer "
-  >
-    View Library
-  </button>
-</div>
+              <div className="w-16 h-16 bg-blue-600/20 rounded-full flex items-center justify-center text-4xl animate-pulse group-hover:animate-none group-hover:scale-110 transition-transform">
+                📝
+              </div>
+              <h2 className="text-3xl font-bold tracking-tight font-[font1]">Manage Notes</h2>
+              <p className="text-gray-300 font-[font1]">Edit details or delete outdated study materials from the library.</p>
+              <button
+                onClick={() => alert("Manage Notes")}
+                className="bg-blue-600 hover:bg-blue-700 px-8 py-3 rounded-lg font-bold transition duration-300 shadow-lg shadow-blue-500/20 font-[font1] w-full cursor-pointer "
+              >
+                View Library
+              </button>
+            </div>
+            <div className="bg-black/70 backdrop-blur-md text-white w-96 p-10 rounded-2xl flex flex-col items-center text-center gap-6 shadow-xl hover:scale-105 transition duration-300 hover:bg-green-900 group  border border-white/5">
+              <div className="w-16 h-16 bg-green-600/20 rounded-full flex items-center justify-center text-4xl animate-pulse group-hover:animate-none group-hover:scale-110 transition-transform">
+                📊
+              </div>
+              <h2 className="text-3xl font-bold tracking-tight font-[font1]">View Stats</h2>
+              <p className="text-gray-300 font-[font1]">Monitor total downloads and see what students are studying most.</p>
+              <button
+                onClick={() => alert("view stats ")}
+                className="bg-green-600 hover:bg-green-700 px-8 py-3 rounded-lg font-bold transition duration-300 shadow-lg shadow-green-500/20 font-[font1] w-full cursor-pointer "
+              >
+                View Analytics
+              </button>
+            </div>
 
 
           </div>
