e76bfb39757ad865b25823eb24ac0d6ab33cad6a
Anshuman Morey <anshumanmorey2@gmail.com>
2026-03-15 18:02:16 +0530

    footer container added

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,10 +1,13 @@
 import { memo } from 'react';
 
 const Footer = () => {
-  return (
-    <div>
-      <h2>Footer</h2>
-    </div>
+return (
+    <footer className="bg-black text-white py-10 mt-auto">
+      <div className="max-w-7xl mx-[50px] px-4 sm:px-6 lg:px-8 grid grid-cols-1 md:grid-cols-3 gap-10">
+        
+        
+      </div>
+    </footer>
   );
 };
 
