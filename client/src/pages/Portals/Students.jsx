bb5024207c89d7ac9c62104ecd8ae8fe4a7efe23
Anshuman Morey <anshumanmorey2@gmail.com>
2026-04-08 14:42:15 +0530

    students page background added

--- client/src/pages/Portals/Students.jsx
+++ client/src/pages/Portals/Students.jsx
@@ -8,6 +8,23 @@ const Students = () => {
     <div className='bg-black w-screen h-screen'>
         <Navbar />
       <h2> Students</h2>
+      <div className="absolute inset-0 z-0 bg-gradient-to-b from-black to-green-900">
+
+  <div className="absolute w-1 h-1 bg-white rounded-full animate-pulse" style={{top:"10%",left:"20%"}}></div>
+  <div className="absolute w-1 h-1 bg-white rounded-full animate-pulse" style={{top:"15%",left:"50%"}}></div>
+  <div className="absolute w-1 h-1 bg-white rounded-full animate-pulse" style={{top:"20%",left:"80%"}}></div>
+  <div className="absolute w-1 h-1 bg-white rounded-full animate-pulse" style={{top:"30%",left:"10%"}}></div>
+  <div className="absolute w-1 h-1 bg-white rounded-full animate-pulse" style={{top:"40%",left:"70%"}}></div>
+  <div className="absolute w-1 h-1 bg-white rounded-full animate-pulse" style={{top:"50%",left:"30%"}}></div>
+  <div className="absolute w-1 h-1 bg-white rounded-full animate-pulse" style={{top:"60%",left:"90%"}}></div>
+  <div className="absolute w-1 h-1 bg-white rounded-full animate-pulse" style={{top:"70%",left:"40%"}}></div>
+  <div className="absolute w-1 h-1 bg-white rounded-full animate-pulse" style={{top:"80%",left:"60%"}}></div>
+  <div className="absolute w-1 h-1 bg-white rounded-full animate-pulse" style={{top:"90%",left:"15%"}}></div>
+
+  <div className="absolute w-2 h-2 bg-purple-300 rounded-full animate-pulse" style={{top:"25%",left:"45%"}}></div>
+  <div className="absolute w-2 h-2 bg-blue-200 rounded-full animate-pulse" style={{top:"55%",left:"75%"}}></div>
+
+</div>
     </div>
     
     </>
