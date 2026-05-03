f7de5dd5959b856c4c7c38285e5177efb3607c0f
Anshuman Morey <anshumanmorey2@gmail.com>
2026-05-03 18:29:18 +0530

    server.js code added

---
+++ server/Server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const uploadRoutes = require("./routes/uploadRoutes");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+
+app.use("/api/notes", uploadRoutes);
+
+
+app.get("/", (req, res) => {
+  res.send("Backend running 🚀");
+});
+
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
