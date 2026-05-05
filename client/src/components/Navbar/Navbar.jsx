43e4e9c88d8eb8c2e25c0397468c5f203787c345
Anshuman Morey <anshumanmorey2@gmail.com>
2026-05-05 16:27:20 +0530

    navbar logout button added for admin login

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,25 +1,39 @@
 import { memo } from 'react';
 import React from 'react';
 import './Navbar.css';
-import { Link } from 'react-router-dom';
+import { Link ,useNavigate } from 'react-router-dom';
 
 
 const Navbar = () => {
+  const navigate = useNavigate();
+  const isAdmin = localStorage.getItem("adminAuth") === "true";
+  const handleLogout = () => {
+    localStorage.removeItem("adminAuth");
+    navigate("/");
+    window.location.reload();
+  };
   return (
     <>
-    
-    <div className=' navbar font-[font1] '>
-      
-      <Link to="/" > <span className='icon-text '>Notes Store</span>  </Link>
-      
-      <Link to="/about" className='bb'>About Us</Link>
-      <Link to="/services" className='bb'>Our Services</Link>
-      <Link to="/contact" className='bb'>Contact Us</Link>
-      <Link to="/Student" className='bb'>Notes</Link>
-      <Link to="/admin-login" className='bb'>Admin Login</Link>
-      
-      
-    </div>
+      <div className='navbar font-[font1]'>
+        <Link to="/"> <span className='icon-text'>Notes Store</span> </Link>
+        
+        <Link to="/about" className='bb'>About Us</Link>
+        <Link to="/services" className='bb'>Our Services</Link>
+        <Link to="/contact" className='bb'>Contact Us</Link>
+        <Link to="/Student" className='bb'>Notes</Link>
+
+        
+        {isAdmin ? (
+          <span 
+            className='bb cursor-pointer text-red-500 font-bold' 
+            onClick={handleLogout}
+          >
+            Logout
+          </span>
+        ) : (
+          <Link to="/admin-login" className='bb'>Admin Login</Link>
+        )}
+      </div>
     </>
   );
 };
