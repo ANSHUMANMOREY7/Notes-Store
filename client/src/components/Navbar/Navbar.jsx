08712c45c7a235cd60f0361e00f6c3bd30abbd2c
Anshuman Morey <anshumanmorey2@gmail.com>
2026-05-07 18:46:49 +0530

    navbar updated

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -15,10 +15,11 @@ const Navbar = () => {
   return (
     <>
       <div className='navbar font-[font1]'>
-        <Link to="/"> <span className='icon-text'>Notes Store</span> </Link>
+        
+        <Link to="/"> 
+        <span className='icon-text'>Notes Store</span> </Link>
         
         <Link to="/about" className='bb'>About Us</Link>
-        <Link to="/services" className='bb'>Our Services</Link>
         <Link to="/contact" className='bb'>Contact Us</Link>
         <Link to="/Student" className='bb'>Notes</Link>
 
